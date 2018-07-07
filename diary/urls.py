25eb8f5d679a0078fb2f62c6c10b293b7062ec9a
MartinStevko <mstevko10@gmail.com>
2018-07-07 16:55:03 +0200

    Diary URL's resolver

---
+++ diary/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, re_path
+
+from . import views
+
+app_name = 'diary'
+
+urlpatterns = [
+]
+
+'''
+    path('obdlznik/login/', views.log_in),
+    path('obdlznik/logout/', views.logout_page),
+    path('obdlznik/logout/request/user/', views.log_out),
+    path('obdlznik/hra/', views.druzinka),
+    path('obdlznik/ulohy/', views.opravovatel),
+    path('obdlznik/spravca/', views.spravca),
+    path('obdlznik/', views.index),
+    re_path(r'^', views.error_404),
+'''
