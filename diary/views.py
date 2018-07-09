58085c7c9aec8837eccde5f40e07dae7a4da0363
MartinStevko <mstevko10@gmail.com>
2018-07-09 16:06:45 +0200

    Index view creation

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    template = 'diary/index.html'
+
+    try:
+        message = request.session['message']
+    except(KeyError):
+        message = None;
+
+    if message:
+        del request.session['message']
+        return render(request, template, {'message':message})
+    else:
+        return render(request, template, {})
+
+def other(request):
+    request.session['message'] = 'Hľadaná stránka nebola nájdená'
+    return redirect('diary:index')
+
+def log_in(request):
+    pass
+
+def log_out(request):
+    pass
+
+def register(request):
+    pass
+
+def profile(request):
+    pass
