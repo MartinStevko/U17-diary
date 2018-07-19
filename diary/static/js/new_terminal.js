3fa7ea7abd56e7f30b8131165283fa5bc452c1da
MartinStevko <mstevko10@gmail.com>
2018-07-19 10:07:31 +0200

    Super redirect

--- diary/static/js/new_terminal.js
+++ diary/static/js/new_terminal.js
@@ -1,33 +1,34 @@
 term = new Terminal(
-    {
+  {
+    type: "POST",
+    ps: '$',
+    greeting: '%+rWith great power comes great responsibilty. %-r%n' +
+              'Released under BSD License. %n(c) 2003-2013 termlib(http://www.masswerk.at/jsuix)%n' +
+              '(c) 2014 Django Console - Anoop Thomas Mathew @atmb4u\n' +
+              'Updated under GPL-3.0 License.%n' +
+              '(c) 2018 Diary Console - Martin Stevko @MartinStevko',
+    id: 1,
+    termDiv: 'termDiv',
+    crsrBlinkMode: true,
+    handler: function () {
+      this.newLine();
+      var line = this.lineBuffer;
+      var universe = this;
+
+      $.ajax({
+        url: 'console/post',
         type: "POST",
-        ps: '$',
-        greeting: '%+rWith great power comes great responsibilty. %-r%n' +
-                'Released under BSD License. %n(c) 2003-2013 termlib(http://www.masswerk.at/jsuix)%n' +
-                '(c) 2014 Django Console - Anoop Thomas Mathew @atmb4u\n' +
-                'Updated under GPL-3.0 License.%n' +
-                '(c) 2018 Diary Console - Martin Stevko @MartinStevko',
-        id: 1,
-        termDiv: 'termDiv',
-        crsrBlinkMode: true,
-        handler: function () {
-            this.newLine();
-            var line = this.lineBuffer;
-            var universe = this;
-            $.ajax({
-                url: 'console/post',
-                type: "POST",
-                data: {
-                    csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
-                    command: line
-                },
-                dataType: "json",
-                complete: function (info) {
-                    universe.write(info.responseText);
-                    universe.prompt();
-                }
-            });
+        data: {
+          csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
+          command: line
+        },
+        dataType: "json",
+        complete: function (info) {
+          universe.write(info.responseText);
+          universe.prompt();
         }
+      });
     }
+  }
 );
 term.open();
