f07cfa304fc5ca7bc0a2607d7b10c6de28cec848
MartinStevko <mstevko10@gmail.com>
2018-07-22 22:19:48 +0200

    Variables for console

---
+++ diary/variables.py
@@ -0,0 +1,3 @@
+directories = ' /home\n /admin/\n /console\n /activity/list\n /diary/list\n /graph\n /diary/logout'
+
+cmd_list = 'Press HOME key or use commands below:\n dir\n active users\n repair profiles\n repair weeks\n help\n exit'
